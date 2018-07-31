e3c772aabfcae0d6936f4939f0ed91359e85c491
Anand Chowdhary <anandchowdhary@gmail.com>
2018-08-01 00:48:08 +0530

    Fix base URL

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Oswald Labs Help' /* title for your website */,
   tagline: 'Agastya, Shravan, and Valmiki',
   url: 'https://your-docusaurus-test-site.com' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  baseUrl: '/docs/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
